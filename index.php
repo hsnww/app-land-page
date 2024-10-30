02d72afafacadd6f623c0f34dbffbb8e670c0237
Hassan Al ZAhrani <hsnwww@gmail.com>
2024-10-30 19:24:03 +0300

    correct welcome page

--- index.php
+++ index.php
@@ -192,11 +192,12 @@
                                 </a>
                             </li>
                             <li class="py-2">
-                                <a class="media" href="mailto:contact@viola.com.sa">
+                                <a class="media" href="mailto:contactus@viola.com.sa">
                                     <div class="social-icon ml-3">
                                         <i class="fas fa-envelope"></i>
                                     </div>
-                                    <span class="media-body align-self-center">contact@viola.com.sa</span>
+                                    <span class="media-body align-self-center">contactus@viola.com.sa
+                                    </span>
                                 </a>
                             </li>
                         </ul>
@@ -236,36 +237,6 @@
                         </ul>
                     </div>
                 </div>
-<!--                <div class="col-12 col-md-6 pt-4 pt-md-0">-->
-<!--                     Contact Box -->
-<!--                    <div class="contact-box text-center">-->
-<!--                         Contact Form -->
-<!--                        <form id="contact-form" method="POST" action="assets/php/mail.php">-->
-<!--                            <div class="row">-->
-<!--                                <div class="col-12">-->
-<!--                                    <div class="form-group">-->
-<!--                                        <input type="text" class="form-control" name="name" placeholder="Name" required="required">-->
-<!--                                    </div>-->
-<!--                                    <div class="form-group">-->
-<!--                                        <input type="email" class="form-control" name="email" placeholder="Email" required="required">-->
-<!--                                    </div>-->
-<!--                                    <div class="form-group">-->
-<!--                                        <input type="text" class="form-control" name="subject" placeholder="Subject" required="required">-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                                <div class="col-12">-->
-<!--                                    <div class="form-group">-->
-<!--                                        <textarea class="form-control" name="message" placeholder="Message" required="required"></textarea>-->
-<!--                                    </div>-->
-<!--                                </div>-->
-<!--                                <div class="col-12">-->
-<!--                                    <button type="submit" class="btn btn-lg btn-block mt-3"><span class="text-white pr-3"><i class="fas fa-paper-plane"></i></span>Send Message</button>-->
-<!--                                </div>-->
-<!--                            </div>-->
-<!--                        </form>-->
-<!--                        <p class="form-message"></p>-->
-<!--                    </div>-->
-<!--                </div>-->
             </div>
         </div>
     </section>
