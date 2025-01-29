c260ea49a91efcfb9c07e5136711469ee77ec739
Hassan Al ZAhrani <hsnwww@gmail.com>
2025-01-29 11:18:15 +0300

    add readme.md file

--- privacy.php
+++ privacy.php
@@ -69,62 +69,143 @@
 
                                         <!-- Card Body -->
                                         <div class="card-body px-0 py-3">
-                                            This Privacy Policy shows how (Viola) uses data of salons & clients who visit our website or use our mobile devices’ applications.  We inform you that your data protection is very important to us, and we understand our responsibilities is to dealing and protecting your data.
+                                            <h3>Viola Privacy Policy</h3>
+
+                                            This Privacy Policy outlines how Viola uses the data of salons and clients who visit our website or use our mobile applications. Your data protection is very important to us, and we understand our responsibilities in dealing with and protecting your data.
                                         </div>
                                         <div class="card-body px-0 py-3">
-                                            For Salons: Viola app uses your data that you entered to reach to your clients like social media sites and contact information.
-                                        </div>
-                                        <div class="card-body px-0 py-3">
-                                            For Clients: Viola app saves you contact to keep you saved favorite salons, and when you choose to apply for jobs with salons, and that will be availed to registered salons only.
-                                        </div>
-                                        <div class="card-body px-0 py-3">
-                                            Other than that, Viola is committed not to used your data for any other uses.
-                                        </div>
-                                        <div class="card-body px-0 py-3">
-                                            Viola uses salons’ and clients’ data for best services provided to both, according to geographic locations in order to list salons according to nearest salons.  And uses contact information in order for clients to reserve services’ appointments and other inquiries.  Even though, clients can benefit from 99% of Viola services without the need to provide any personal data (just surfing as a guest).
-                                        </div>
-                                        <div class="card-body px-0 py-3">
-                                            Viola is committed to not providing or publicizing any information and data for both salons and clients to any other third parties for any reason.
-                                        </div>
-                                        <div class="card-body px-0 py-3">
-                                            Viola is committed to use best services provides to host their servers, the ones with highest trusted sites, who apply best standards of data security.
-                                        </div>
-                                        <div class="card-body px-0 py-3">
-                                            Viola is committed not to detect any of salons’ or clients’ data other than what is entered by themselves, not detect their device type, IP address, operating systems and their versions, programs, languages, nor any other data or network information.
-                                        </div>
+
+                                            <h3> For Salons:</h3>
+                                            Viola app uses the data you provide to reach your clients, such as social media links and contact information.
+                                            </div>
+                                            <div class="card-body px-0 py-3">
+
+                                                <h3> For Clients:</h3>
+                                            Viola app saves your contact information to keep a record of your favorite salons and facilitates job applications with salons, which will be accessible only to registered salons. Other than that, Viola is committed to not using your data for any other purposes.
+                                                </div>
+                                                <div class="card-body px-0 py-3">
+
+                                            Viola uses the data of salons and clients to provide the best services to both, according to geographic locations. This allows us to list salons based on proximity. Contact information is used to enable clients to reserve appointments and make other inquiries. Even so, clients can benefit from 99% of Viola’s services without needing to provide any personal data (just browsing as a guest).
+                                                    </div>
+                                                    <div class="card-body px-0 py-3">
+
+                                            Viola is committed to not sharing or publicizing any information or data of salons and clients to any third parties for any reason.
+                                                        </div>
+                                                        <div class="card-body px-0 py-3">
+
+                                            Viola is committed to using the best service providers to host its servers, selecting those with the highest trust ratings and best data security standards.
+                                                            </div>
+                                                            <div class="card-body px-0 py-3">
+
+                                            Viola is committed to not detecting any data from salons or clients beyond what is explicitly entered by them. This includes refraining from collecting information about device type, IP address, operating systems and their versions, programs, languages, or any other data or network information.
+
+                                                                </div>
+                                                                <div class="card-body px-0 py-3">
+
+                                                                    <h3> Geolocation Data Use and Privacy</h3>
+                                                                    </div>
+                                                                    <div class="card-body px-0 py-3">
+
+                                            Viola app may request access to your location data to provide enhanced features, such as listing salons based on proximity and allowing clients to discover nearby services. For these features to function optimally, the app may require access to your location, even when the app is running in the background. We are committed to ensuring this data is handled responsibly and transparently.
+                                                                        </div>
+                                                                        <div class="card-body px-0 py-3">
+
+                                                                            <h4> How We Use Geolocation Data:</h4>
+                                            1. Service Optimization: Geolocation data helps us suggest salons and services nearest to your current location, enhancing your experience.<br>
+                                            2. Appointment Scheduling and Recommendations: Location information is used to connect clients with nearby salons for efficient scheduling.<br>
+                                                                            </div>
+                                                                            <div class="card-body px-0 py-3">
+
+                                                                                <h4> User Consent and Control:  </h4>
+                                            - Viola only accesses location data after receiving explicit consent from the user.<br>
+                                            - Users have the option to enable or disable location tracking at any time via their device settings.<br>
+                                            - Background location access is solely used to support the aforementioned features and will not be used for any unauthorized purposes.<br>
+                                                                                </div>
+                                                                                <div class="card-body px-0 py-3">
+
+                                                                                    <h4> Data Security: </h4>
+                                            - Viola does not store geolocation data on our servers beyond what is necessary to provide the requested service.<br>
+                                            - All location data is transmitted securely and is not shared with third parties without the user’s explicit consent.<br>
+                                                                                    </div>
+
+                                                                                    <div class="card-body px-0 py-3">
+
+                                            By ensuring transparency and adhering to these principles, Viola complies with geolocation permissions policies and prioritizes user privacy.
+                                         </div>
                                         <div class="card-body px-0 py-3">
                                         </div>
                                     </div>
                                 </div>
-                                <div class="col-12 col-md-6 text-right">
+                                <div class="col-12 col-md-6 text-right" style="direction: rtl">
                                     <!-- Single Card -->
                                     <div class="card border-0">
                                         <!-- Card Body -->
+                                        <!-- محتوى البطاقة -->
                                         <div class="card-body px-0 py-3">
-                                            سياسة الخصوصية لـ (فيولا)
-                                            26 أكتوبر 2024
+                                            <h3>سياسة الخصوصية لتطبيق Viola</h3>
 
+                                            توضح سياسة الخصوصية هذه كيفية استخدام Viola لبيانات الصالونات والعملاء الذين يزورون موقعنا الإلكتروني أو يستخدمون تطبيقاتنا. حماية بياناتك مهمة جدًا بالنسبة لنا، وندرك مسؤولياتنا في التعامل مع بياناتك وحمايتها.
                                         </div>
                                         <div class="card-body px-0 py-3">
-                                            توضح سياسة الخصوصية هذه كيفية قيام (فيولا) باستخدام البيانات الشخصية للصالونات وعملاء الصالونات الذين يزورون موقعنا الإلكتروني أو يتلقون خدمات من خلال أي من التطبيقات المحمولة.  نشعركم بأن حماية بياناتك الشخصية أمر في غاية الأهمية بالنسبة لنا، وندرك مسؤوليتنا في التعامل مع بياناتك الشخصية بعناية والحفاظ على أمانها.
-                                        </div>
-                                        <div class="card-body px-0 py-3">
-                                            بالنسبة للصالونات: تُستخدم بياناتك التي أدخلتها أنت في التطبيق لتصل إلى عملائك مثل مواقع التواصل الاجتماعي وأرقام التواصل معكم.  بالنسبة للعملاء: تُستخدم بياناتك لحفظ معلوماتك مثل الصالونات المفضلة والتقديم على الوظائف في الصالونات، وتتاح البيانات فقط لأصحاب الصالونات.
-                                            وفيما عدا ذلك تلتزم (فيولا) بعدم الاستخدامات الأخرى لأي أطراف أخرى.
 
+                                            <h3>للصالونات:</h3>
+                                            يستخدم تطبيق Viola البيانات التي تقدمها للوصول إلى عملائك، مثل روابط وسائل التواصل الاجتماعي ومعلومات الاتصال.
                                         </div>
                                         <div class="card-body px-0 py-3">
-                                            تستخدم (فيولا) البيانات للصالونات والعملاء لتقديم خدمة أفضل حسب الموقع الجغرافي لتوضيح مواقع الصالونات المجاورة والقريبة.  وتستخدم أرقام التواصل ليتم التواصل مع الصالونات للاستفسارات وعمل الحجوزات.  مع العلم بأن العملاء يستطيعون الاستفادة من 99% من الخدمات دون تسجيل أي من البيانات (متصفح فقط كزائر).
+
+                                            <h3>للعملاء:</h3>
+                                            يقوم تطبيق Viola بحفظ معلومات الاتصال الخاصة بك للاحتفاظ بسجل للصالونات المفضلة لديك وتسهيل التقديم على الوظائف في الصالونات، والتي ستكون متاحة فقط للصالونات المسجلة. بخلاف ذلك، يلتزم Viola بعدم استخدام بياناتك لأي أغراض أخرى.
                                         </div>
                                         <div class="card-body px-0 py-3">
-                                            تلتزم (فيولا) بعدم تقديم أو نشر المعلومات الخاصة للصالونات والعملاء لأي طرف آخر لأي غرض كان.
+
+                                            يستخدم Viola بيانات الصالونات والعملاء لتقديم أفضل الخدمات لكليهما، بناءً على المواقع الجغرافية. يتيح لنا ذلك عرض الصالونات الأقرب إليك. يتم استخدام معلومات الاتصال لتمكين العملاء من حجز المواعيد والاستفسارات الأخرى. ومع ذلك، يمكن للعملاء الاستفادة من 99٪ من خدمات Viola دون الحاجة إلى تقديم أي بيانات شخصية (من خلال التصفح كضيف فقط).
                                         </div>
                                         <div class="card-body px-0 py-3">
-                                            تلتزم (فيولا) باستخدام خدمات استضافة للسيرفرات، من مزودي الخدمات الموثوقين والذين يطبقون أعلى معاير أمن المعلومات.
+
+                                            يلتزم Viola بعدم مشاركة أو نشر أي معلومات أو بيانات تخص الصالونات والعملاء مع أي جهة خارجية لأي سبب من الأسباب.
                                         </div>
                                         <div class="card-body px-0 py-3">
-                                            توضح (فيولا) بأنها لا تلتقط من الصالونات والعملاء أي بيانات غير المفصح عنها من قبلهم، مثل (فيولا) لا تلتقط نوع الجهاز ولا عنوان الـ IP الخاص بالجهاز ولا أنظمة التشغيل والإصدارات والبرمجيات واللغات ومعرفات البيانات الأخرى ولا بيانات الشبكة المحمولة.
+
+                                            يلتزم Viola باستخدام أفضل مزودي الخدمة لاستضافة خوادمه، مع اختيار مقدمي الخدمات الأعلى تصنيفًا والأكثر أمانًا من حيث معايير حماية البيانات.
                                         </div>
+                                        <div class="card-body px-0 py-3">
+
+                                            يلتزم Viola بعدم جمع أي بيانات من الصالونات أو العملاء بخلاف ما يتم إدخاله صراحةً من قبلهم. يشمل ذلك الامتناع عن جمع معلومات مثل نوع الجهاز، وعنوان IP، وأنظمة التشغيل وإصداراتها، والبرامج، واللغات، أو أي بيانات أخرى متعلقة بالشبكة.
+                                        </div>
+                                        <div class="card-body px-0 py-3">
+
+                                            <h3>استخدام بيانات الموقع الجغرافي والخصوصية</h3>
+                                        </div>
+                                        <div class="card-body px-0 py-3">
+
+                                            قد يطلب تطبيق Viola الوصول إلى بيانات موقعك الجغرافي لتوفير ميزات محسنة، مثل عرض الصالونات القريبة منك والسماح لك باكتشاف الخدمات القريبة. لكي تعمل هذه الميزات بشكل مثالي، قد يتطلب التطبيق الوصول إلى موقعك حتى أثناء تشغيله في الخلفية. نحن ملتزمون بضمان التعامل مع هذه البيانات بمسؤولية وشفافية.
+                                        </div>
+                                        <div class="card-body px-0 py-3">
+
+                                            <h4>كيفية استخدام بيانات الموقع الجغرافي:</h4>
+                                            1. تحسين الخدمة: تساعد بيانات الموقع الجغرافي في اقتراح الصالونات والخدمات الأقرب إلى موقعك الحالي، مما يعزز تجربتك.<br>
+                                            2. جدولة المواعيد والتوصيات: يتم استخدام معلومات الموقع لربط العملاء بالصالونات القريبة لتسهيل عمليات الحجز.<br>
+                                        </div>
+                                        <div class="card-body px-0 py-3">
+
+                                            <h4>موافقة المستخدم والتحكم:</h4>
+                                            - لا يصل Viola إلى بيانات الموقع الجغرافي إلا بعد الحصول على موافقة صريحة من المستخدم.<br>
+                                            - يمكن للمستخدمين تمكين أو تعطيل تتبع الموقع في أي وقت عبر إعدادات أجهزتهم.<br>
+                                            - يتم استخدام الوصول إلى الموقع في الخلفية فقط لدعم الميزات المذكورة أعلاه، ولن يتم استخدامه لأي أغراض غير مصرح بها.<br>
+                                        </div>
+                                        <div class="card-body px-0 py-3">
+
+                                            <h4>أمان البيانات:</h4>
+                                            - لا يقوم Viola بتخزين بيانات الموقع الجغرافي على خوادمه أكثر مما هو ضروري لتقديم الخدمة المطلوبة.<br>
+                                            - يتم نقل جميع بيانات الموقع بأمان، ولا تتم مشاركتها مع أي جهات خارجية دون موافقة صريحة من المستخدم.<br>
+                                        </div>
+
+                                        <div class="card-body px-0 py-3">
+
+                                            من خلال ضمان الشفافية والالتزام بهذه المبادئ، يلتزم Viola بسياسات أذونات الموقع الجغرافي ويعطي الأولوية لخصوصية المستخدم.
+                                        </div>
+                                        <div class="card-body px-0 py-3">
+
                                         <div class="card-body px-0 py-3">
                                         </div>
 
